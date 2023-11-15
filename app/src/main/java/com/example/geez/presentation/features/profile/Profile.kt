0b246abee37b66f70af208c765e91f7eed6b9fe3
mfadlihs <winnerfadli999@gmail.com>
2023-11-15 22:06:51 +0700

    profile page

--- app/src/main/java/com/example/geez/presentation/features/profile/Profile.kt
+++ app/src/main/java/com/example/geez/presentation/features/profile/Profile.kt
@@ -1,6 +1,8 @@
 package com.example.geez.presentation.features.profile
 
-import android.annotation.SuppressLint
+import AboutProfile
+import AccountSetting
+import HelpCenter
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -8,15 +10,19 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -30,6 +36,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -40,7 +47,7 @@ import com.example.geez.R
 @Preview
 @Composable
 fun Profile() {
-    Scaffold(
+    return Scaffold(
         topBar = {
             TopAppBar(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
@@ -78,6 +85,7 @@ fun Profile() {
             modifier = Modifier
                 .padding(innerPadding)
                 .padding(horizontal = 20.dp, vertical = 16.dp)
+                .verticalScroll(rememberScrollState())
         ) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Box(
@@ -130,6 +138,55 @@ fun Profile() {
                         )
                     }
                 }
+            }
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color(0xff449EEE), shape = RoundedCornerShape(5.dp))
+                    .padding(end = 12.dp),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.chips),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .height(50.dp)
+                        .width(80.dp),
+                    contentScale = ContentScale.Fit
+                )
+                Text(
+                    text = "Buy additional Chips here!",
+                    style = TextStyle(
+                        color = Color(0xffF9FAFB),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 13.sp,
+                        textAlign = TextAlign.End,
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)
+                        .padding(end = 12.dp),
+                )
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowRight,
+                    contentDescription = null, // Add appropriate content description
+                    tint = Color.White
+                )
+            }
+            AccountSetting()
+            HelpCenter()
+            AboutProfile()
+            Button(onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0xFFFF574D)),
+                shape = RoundedCornerShape(14.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 30.dp)
+                    .height(50.dp)
+            ) {
+                Text(text = "Log Out", fontSize = 20.sp)
             }
         }
     }
