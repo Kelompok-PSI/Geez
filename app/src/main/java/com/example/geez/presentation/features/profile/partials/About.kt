92a78a90cb5f7cca25a4c34c14e10ea62fc367f9
Muhammad Fadli Hasan <87264385+mfadlihs@users.noreply.github.com>
2023-11-15 22:07:37 +0700

    Fadli - Profile Page (#3)
    * profile page header
    
    * profile page

---
+++ app/src/main/java/com/example/geez/presentation/features/profile/partials/About.kt
@@ -0,0 +1,99 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.CheckCircle
+import androidx.compose.material.icons.outlined.LocationOn
+import androidx.compose.material.icons.outlined.Lock
+import androidx.compose.material.icons.rounded.KeyboardArrowRight
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.geez.R
+
+@Composable
+fun AboutProfile() {
+    val bTextStyle = TextStyle(
+        fontSize = 12.sp,
+        fontWeight = FontWeight.W600,
+        color = Color(0xff1C222B),
+    )
+
+    return Column() {
+        Text(
+            text = "About",
+            style = TextStyle(
+                fontSize = 14.sp,
+                fontWeight = FontWeight.W800,
+                color = Color(0xff1C222B)
+            ),
+            modifier = Modifier.padding(bottom = 8.dp, top = 24.dp)
+        )
+        Column(
+            modifier = Modifier
+                .padding(vertical = 2.dp, horizontal = 12.dp)
+                .background(color = Color.White, shape = RoundedCornerShape(5.dp))
+                .fillMaxWidth()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.info),
+                        contentDescription = "",
+                        modifier = Modifier.padding(end = 18.dp).size(18.dp),
+                    )
+                    Text(text = "About Us", style = bTextStyle)
+                }
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowRight,
+                    contentDescription = null, // Add appropriate content description
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+            Divider(thickness = 1.dp, color = Color(0xff8897AE))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+            ) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.privacy_policy),
+                        contentDescription = "",
+                        modifier = Modifier.padding(end = 18.dp).size(18.dp),
+                    )
+                    Text(text = "Privacy Policy", style = bTextStyle)
+                }
+                Icon(
+                    imageVector = Icons.Rounded.KeyboardArrowRight,
+                    contentDescription = null, // Add appropriate content description
+                    tint = Color.Black,
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        }
+    }
+}
