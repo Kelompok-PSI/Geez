d3ceaeffe7bb5431164a3903575aa092f5bb5881
Daffadon <daffaputranarendra9@gmail.com>
2023-11-15 22:42:54 +0700

    UI: FormDonation Created

--- app/src/main/java/com/example/geez/presentation/navigation/Screen.kt
+++ app/src/main/java/com/example/geez/presentation/navigation/Screen.kt
@@ -3,6 +3,7 @@ package com.example.geez.presentation.navigation
 sealed class Screen(val route: String){
     object CampaignList : Screen("campaign_list")
     object CampaignDetail : Screen("campaign_detail")
+    object FormDonation : Screen("form_donation")
 
     fun withArgs(vararg args: String): String{
         return buildString {
