7159eee666a2609f28c3e665fc2b4a272823e8e3
Muhammad Fadli Hasan <87264385+mfadlihs@users.noreply.github.com>
2023-11-16 22:55:09 +0700

    [FADLI] Onboarding Screen & Splash Screen (#5)
    * splash page
    
    * add font
    
    * onboarding page
    
    * onbaording

---
+++ app/src/main/java/com/example/geez/presentation/features/splash/Splash.kt
@@ -0,0 +1,50 @@
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.geez.presentation.ui.theme.AppFont
+
+@Preview
+@Composable
+fun SplashPage() {
+    Column(
+        modifier = Modifier
+            .background(
+                brush = Brush.linearGradient(
+                    colors = listOf(
+                        Color(0xff449EEE),
+                        Color(0xff8FC5F5),
+                    ),
+                    start = Offset(0f, 100f),
+                )
+            )
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "GEEZ",
+            style = TextStyle(
+                color = Color.White,
+                fontSize = 64.sp,
+                fontFamily = AppFont.montserrat
+            ),
+        )
+    }
+}
--- app/src/main/java/com/example/geez/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/geez/presentation/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.example.geez.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.geez.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -32,3 +34,9 @@ val Typography = Typography(
     )
     */
 )
+
+object AppFont {
+    var montserrat = FontFamily(
+        Font(R.font.montserrat)
+    )
+}
