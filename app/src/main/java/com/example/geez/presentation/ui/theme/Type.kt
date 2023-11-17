7c09b3d4fa0c5d4e178893429769fc7d8766ed8a
Daffadon <daffaputranarendra9@gmail.com>
2023-11-17 23:46:56 +0700

    rebase

--- app/src/main/java/com/example/geez/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/example/geez/presentation/ui/theme/Type.kt
@@ -3,9 +3,11 @@ package com.example.geez.presentation.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.geez.R
 import com.example.geez.R
 
 // Set of Material typography styles to start with
