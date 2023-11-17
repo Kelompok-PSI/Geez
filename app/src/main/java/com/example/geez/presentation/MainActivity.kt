bc4386eea13041f09802674917b82380b964be09
FirH <firhanimamhaekal@gmail.com>
2023-11-17 17:02:31 +0700

    Login, Big Party Register, Regular Register

--- app/src/main/java/com/example/geez/presentation/MainActivity.kt
+++ app/src/main/java/com/example/geez/presentation/MainActivity.kt
@@ -5,13 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.geez.presentation.features.campaignDetails.CampaignDetail
 import com.example.geez.presentation.features.campaignList.CampaignList
+import com.example.geez.presentation.features.login.Login
+import com.example.geez.presentation.features.registerBigParty.registerBigParty
 import com.example.geez.presentation.navigation.Navigation
+import com.example.geez.presentation.features.registerReguler.registerReguler
+import com.example.geez.presentation.features.registerReguler.registerReguler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+//            registerBigParty()
+//            registerReguler()
+            Login()
+//            Navigation()
 //            CampaignList()
 //            CampaignDetail()
         }
